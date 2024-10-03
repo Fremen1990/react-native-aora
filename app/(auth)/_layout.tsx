793f17978968631aae78d3b6ad1d689c32e39ff4
Tomasz Stanisz <tomasz.stanisz@orange.com>
2024-10-03 22:17:35 +0100

    Implement SignUp and SignIn

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import {Text, View} from 'react-native';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+// TODO: 1:30:00
+// https://www.youtube.com/watch?v=ZBCUegTZF7M&ab_channel=JavaScriptMastery
 
 const AuthLayout = () => {
-    return (
-        <View>
-            <Text>
-                Auth Layout
-            </Text>
-        </View>
-    );
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
 };
 
 export default AuthLayout;
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,15 +1,79 @@
-import React from 'react';
+import React from "react";
 
-import {Text, View} from 'react-native';
+import { ScrollView, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const SignIN = () => {
-    return (
-        <View>
-            <Text>
-            SignIn
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+
+const SignIn = () => {
+  const [form, setForm] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  const submit = () => {
+    setIsSubmitting(true);
+    alert("Submitted");
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+            title={"Email"}
+            value={form.email}
+            handleChangeText={(text: string) =>
+              setForm({ ...form, email: text })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title={"Password"}
+            value={form.password}
+            handleChangeText={(text: string) =>
+              setForm({ ...form, password: text })
+            }
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
             </Text>
+
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign Up
+            </Link>
+          </View>
         </View>
-    );
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
-export default SignIN;
+export default SignIn;
